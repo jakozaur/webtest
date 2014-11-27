11dadb575d69cbf0d4e3f09352e04e8a06221ec4
Jacek Migdal <jacek@migdal.pl>
2014-11-27 15:47:04 -0800

    Nice JavaScript editor

--- client/phantomjsCode.js
+++ client/phantomjsCode.js
@@ -1,3 +1,36 @@
+Template.phantomjsCode.helpers({
+  options: function () {
+    return {
+      mode: 'javascript',
+      lineNumbers: true
+    };
+  },
+  code: function () {
+    return "\
+var page = require('webpage').create();\n\
+\n\
+page.viewportSize = viewportSize;\n\
+\n\
+page.open(\"http://google.com\", function (status) {\n\
+  //Page is loaded!\n\
+  console.log(\"Page Opened!\");\n\
+  var base64 = page.renderBase64('PNG');\n\
+  var title = page.evaluate(function () {\n\
+    return document.title;\n\
+  });\n\
+\n\
+  var result = {\n\
+    'screenshootPngBase64': base64,\n\
+    'title': title\n\
+  };\n\
+\n\
+  callback(undefined, result);\n\
+  //phantom.exit();\n\
+});\n\
+    ";
+  }
+});
+
 Template.phantomjsCode.events({
   'click button': function (event, tmpl) {
     var code = tmpl.$('textarea').val();
