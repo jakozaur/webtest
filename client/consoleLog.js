897d346b9f32143c53c72efc78488fde252dcc7e
Malgorzata Migdal <malgorzata@migdal.pl>
2014-11-28 12:40:07 -0800

    All of the icons styled nicely. Fonts work as well

--- client/consoleLog.js
+++ client/consoleLog.js
@@ -5,3 +5,9 @@ Template.consoleLog.helpers({
     return Session.get('logs');
   }
 });
+
+Template.consoleLogMessage.helpers({
+  typeClass: function() {
+    return this.type.replace(' ', '-');
+  }
+});
